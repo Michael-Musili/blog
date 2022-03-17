43fd6adf1288bd06a6ab14e352ca357e57a03e82
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-17 13:55:36 +0300

    commit changes

--- app/config.py
+++ app/config.py
@@ -2,25 +2,26 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:access@localhost/dbblogapp'
+    SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'mike'
     
     
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:access@localhost/dbblogapp'
+    SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
     DEBUG = True
 
 
 class ProdConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
     DEBUG = False
     
 config_options = {
     'development': DevConfig,
     'production': ProdConfig,
 }
+
 
 
 
